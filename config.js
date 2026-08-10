bcf7ff0aa2eca02e7ce8729647d613425691bd4f
Vitor Ivens <vitorivens@gmail.com>
2026-08-10 15:04:18 -0300

    Configure Neon Auth and Data API for GitHub Pages

---
+++ config.js
@@ -0,0 +1,6 @@
+window.DW_CONFIG = Object.freeze({
+  authUrl: "https://ep-silent-wave-axorgrza.neonauth.c-4.us-east-2.aws.neon.tech/neondb/auth",
+  dataApiUrl: "https://ep-silent-wave-axorgrza.apirest.c-4.us-east-2.aws.neon.tech/neondb/rest/v1",
+  gameplayPollMs: 750,
+  presencePollMs: 5000
+});
